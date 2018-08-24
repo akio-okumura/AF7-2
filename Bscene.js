f87de3a677e90497af8663670bd9341a1fe3ba39
akio <nrtrstar@yahoo.co.jp>
2018-08-25 01:00:16 +0900

    switch文追加

--- Bscene.js
+++ Bscene.js
@@ -6,9 +6,22 @@ var p2 = document.querySelector('#page2');
 var p_num = 1;
 
 col.addEventListener('click', function () {
-  if(p_num == 1) {
-    p1.setAttribute('visible', 'false'); // p1を消す
-    p2.setAttribute('visible', 'true');  // p2を出す
-    p_num = 2;
+  switch (p_num) {
+    case 1: p1.setAttribute('visible', 'false'); // p1を消す
+            p2.setAttribute('visible', 'true');  // p2を出す
+            p_num = 2;
+
+            break;
+    case 2: p2.setAttribute('visible', 'false'); // p2を消す
+            p3.setAttribute('visible', 'true');  // p3を出す
+            p_num = 3;
+
+            break;
+
+    default:  break;
+
+
   }
+
+
 });
