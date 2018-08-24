e6642f49e799dc822cb5ecf8d1e8534b23e65e8a
akio <nrtrstar@yahoo.co.jp>
2018-08-25 01:02:55 +0900

    coll delete

--- Bscene.js
+++ Bscene.js
@@ -1,11 +1,11 @@
 var tb = document.querySelector('#textboard'); // テキストボード
-var col = document.querySelector('#board_coll'); // ボードのコライダー
+//var col = document.querySelector('#board_coll'); // ボードのコライダー
 var p1 = document.querySelector('#page1');
 var p2 = document.querySelector('#page2');
 
 var p_num = 1;
 
-col.addEventListener('click', function () {
+tb.addEventListener('click', function () {
   switch (p_num) {
     case 1: p1.setAttribute('visible', 'false'); // p1を消す
             p2.setAttribute('visible', 'true');  // p2を出す
