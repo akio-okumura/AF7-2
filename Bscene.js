50a5c7c952209beaff8fb9f6d18a655e8ca32827
akio <nrtrstar@yahoo.co.jp>
2018-08-25 00:56:33 +0900

    col add

--- Bscene.js
+++ Bscene.js
@@ -1,10 +1,11 @@
 var tb = document.querySelector('#textboard'); // テキストボード
+var col = document.querySelector('#board_coll'); // ボードのコライダー
 var p1 = document.querySelector('#page1');
 var p2 = document.querySelector('#page2');
 
 var p_num = 1;
 
-tb.addEventListener('click', function () {
+col.addEventListener('click', function () {
   if(p_num == 1) {
     p1.setAttribute('visible', 'false'); // p1を消す
     p2.setAttribute('visible', 'true');  // p2を出す
